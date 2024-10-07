623fcd22f9861d9cb5c49ce0af2810f9d4d178bb
Musab Momin <mominmusab127@gmail.com>
2024-10-07 20:42:37 +0530

    feat: init advance jpa project

---
+++ advance-jpa/src/main/java/com/learningjpa/advance_jpa/AdvanceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learningjpa.advance_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceJpaApplication.class, args);
+	}
+
+}
---
+++ advance-jpa/src/test/java/com/learningjpa/advance_jpa/AdvanceJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningjpa.advance_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
